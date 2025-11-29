6e684ab3f8ce15232aed2293b6c2e42700fd511f
sachinjain-hub <sj314195@gmail.com>
2025-11-29 23:15:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_PORT'] = (os.environ.get('MYSQL_PORT'))
 # Flask-WTF / Session secret key
 app.secret_key = 'sk'
 
